22a11d68e3a9196c4818f1f795a91a1458245932
Maxime Vautier <vautier_maxime@yahoo.com>
2021-04-20 10:17:49 +0200

    Correction des erreurs AnalyticsCounter.java Génération correcte du fichier result.out

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -9,9 +9,9 @@ public class AnalyticsCounter {
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
 	
-	public static void main(String args[]) throws Exception {
+	public static void main(String[] args) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
 
 		int i = 0;	// set i to 0
@@ -21,9 +21,10 @@ public class AnalyticsCounter {
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headCount++;
+				headacheCount++;
 				System.out.println("number of headaches: " + headCount);
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) {
 				rashCount++;
 			}
 			else if (line.contains("pupils")) {
