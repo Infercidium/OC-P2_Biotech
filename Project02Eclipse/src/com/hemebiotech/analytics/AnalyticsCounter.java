356f42685130939e7b5a8edf116ebd5906ad339d
Maxime Vautier <vautier_maxime@yahoo.com>
2021-04-22 16:08:37 +0200

    *Correction de la manière de faire après discution avec Mentor  *Création d'une classe Main  *Utilisation de ReadSymptomDataFromFile.java et de son Interface ISymptomReader.java  *Integration des méthodes des classes à supprimer dans AnalyticsCounter.java  *Suppression des Classes inutilisées  *Ajout de toLowerCase dans ReadSymptomDataFromFile.java pour optimiser la gestion des doublons  *Ajout d'un "titre" et d'un "resumé" dans la méthode writter d' AnalyticsCounter.java pour rendre plus lisible result.out

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,21 +1,62 @@
 package com.hemebiotech.analytics;
 
-import com.hemebiotech.analytics.tools.Sorter;
-import com.hemebiotech.analytics.tools.Writter;
-
-import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter
 {
-	public static void main(String[] args)
-	{
-		String fichier = "Project02Eclipse/symptoms.txt";
-		ArrayList<String> symptoms = new ArrayList<>();
-		Sorter sorter = new Sorter();
-		Writter writter = new Writter();
+    /**
+     *
+     * @param result a list of unsorted symptoms
+     * @return a list of all symptoms by ascii order duplicates are possible / probable
+     */
+    public List<String> sorter(List<String> result)
+    {
+        Collections.sort(result);
+        return result;
+    }
 
-		sorter.sorter(symptoms, fichier);
-		writter.written(symptoms, fichier);
+    /**
+     *
+     * @param result a list of symptoms
+     * @return a map of symptoms, map key is the symptom and map value is the occurence
+     */
+    public Map<String, Integer> counter(List<String> result) {
+        Map<String, Integer> counter = new TreeMap<>();
+        for (String s : result) {
+            if (counter.containsKey(s)) {
+                counter.put(s, counter.get(s) + 1);
+            } else {
+                counter.put(s, 1);
+            }
+        }
+        return counter;
+    }
 
-	}
+    /**
+     *
+     * @param counter a map of symptoms
+     */
+   public void writter(Map<String, Integer> counter)
+    {
+        try {
+            FileWriter writer = new FileWriter("result.out");
+            writer.write("List of Symptom(s): \n\n");
+            for (Map.Entry<String, Integer> symptoms : counter.entrySet())
+            {
+                writer.write(symptoms.getKey() + " = " + symptoms.getValue() + "\n");
+                totalSympt += symptoms.getValue();
+            }
+            writer.write("\n Different symptom(s) = " + counter.size() + "\n Number of symptom(s) = " + totalSympt);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Unable to create file or written");
+        }
+    }
+    private int totalSympt = 0;
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,16 @@
+package com.hemebiotech.analytics;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        //Définition du chemin
+        String fichier = "Project02Eclipse/symptoms.txt";
+
+        //Instantiation
+        ISymptomReader reader = new ReadSymptomDataFromFile(fichier);
+        AnalyticsCounter analitics = new AnalyticsCounter();
+
+        //Déroulement -> symptoms.txt > reader > sorter > counter > writter > result.out
+        analitics.writter(analitics.counter(analitics.sorter(reader.GetSymptoms())));
+    }
+}
